b091feeb26facc9ccc9d92ca5083526dc262535c
AnilKumar1127 <42654091+AnilKumar1127@users.noreply.github.com>
2019-10-21 21:08:32 +0000

    working split N-ways Tip Calc

--- src/apps.ts
+++ src/apps.ts
@@ -1,17 +1,22 @@
-import { getTotalAmtWithTip } from './utils';
+import { getTotalAmtWithTip, tipCal } from './utils';
 
 let bill = 0;
 let inputSum: HTMLInputElement;
 let custTipAmt: HTMLInputElement;
 let custTipBt: HTMLElement;
+let tipSlpitInput: HTMLInputElement;
 let seletdTip: NodeListOf<HTMLButtonElement>;
+let calTipAmt = 0;
+let userTipAmt = 0;
 const grndTotal = document.getElementById('grndtotal');
-// let sum: number;
+const tipAmt = document.getElementById('tipAmt');
+const sptAmtInto = document.getElementById('amoutSplit');
 export function runApp() {
     seletdTip = document.querySelectorAll('button');
     inputSum = document.getElementById('totalAmt') as HTMLInputElement;
     custTipAmt = document.getElementById('custmAmt') as HTMLInputElement;
     custTipBt = document.getElementById('custTip');
+    tipSlpitInput = document.getElementById('tipSlpit') as HTMLInputElement;
     seletdTip.forEach(el => el.addEventListener('click', handleClick));
     custTipBt.addEventListener('click', custTipCal);
 }
@@ -28,13 +33,25 @@ function handleClick() {
     } else if (that.innerText === '%20') {
         numTip = 20;
     }
-    const x = getTotalAmtWithTip(numTip, bill);
-    grndTotal.innerText = 'Total Amount:$' + x.toString();
+    calTipAmt = getTotalAmtWithTip(numTip, bill);
+    userTipAmt = tipCal(numTip, bill);
+    tipAmt.innerText = 'Amount of tip:$' + userTipAmt.toString();
+    grndTotal.innerText = 'Total Amount:$' + calTipAmt.toString();
+
+    const nway = tipSlpitInput.valueAsNumber;
+    const youPay = (calTipAmt / nway);
+    sptAmtInto.innerText = 'Amout is Split into"' + nway + '"ways:$' + youPay.toString();
 }
 
 function custTipCal() {
     const nowTip = custTipAmt.valueAsNumber;
-    const x = getTotalAmtWithTip(nowTip, bill);
-    grndTotal.innerText = 'Total Amount:$' + x.toString();
+    calTipAmt = getTotalAmtWithTip(nowTip, bill);
+    userTipAmt = tipCal(nowTip, bill);
+    tipAmt.innerText = 'Amount of tip:$' + userTipAmt.toString();
+    grndTotal.innerText = 'Total Amount:$' + calTipAmt.toString();
+    const nway = tipSlpitInput.valueAsNumber;
+    const youPay = (calTipAmt / nway);
+    sptAmtInto.innerText = 'Amout is Split into"' + nway + '"ways:$' + youPay.toString();
 }
+
 runApp();
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 export function getTotalAmtWithTip(tipPersnt: number, totAmt: number) {
     return ((tipPersnt * totAmt) / 100 + totAmt);
 }
+export function tipCal(tipPer: number, billAmt: number) {
+    return ((tipPer * billAmt) / 100);
+}
